5c5aceeb92d3e2185a2b3af01bd5ae46e619ec60
Clément Darne <clementdarne8888@gmail.com>
2022-02-07 18:16:00 -0500

    Updated .gitignore and fixed premake5.lua to support graphics.

--- premake5.lua
+++ premake5.lua
@@ -1,38 +1,85 @@
+newoption {
+	trigger = "sfml-audio",
+	description = "Add audio support to SFML."
+}
+
+newoption {
+	trigger = "sfml-network",
+	description = "Add network support to SFML."
+}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+
 project "SFML"
 	kind "StaticLib"
 	language "C++"
+	cppdialect "C++20"
 	staticruntime "on"
 
 	targetdir("bin/" .. outputdir .. "/%{prj.name}")
 	objdir("bin-int/" .. outputdir .. "/%{prj.name}")
-		
+	
+	includedirs {
+		"extlibs/headers/",
+		"extlibs/headers/glad/include",
+		"extlibs/headers/vulkan/",
+		"extlibs/headers/freetype2/",
+		"extlibs/headers/stb_image/",
+		"include/",
+		"src/"
+	}
+
 	files {
 		"include/**.hpp",
 		"src/**.hpp",
 		"src/**.cpp",
 	}
 
+	removefiles {
+		"src/SFML/Main/**",
+		"src/SFML/Window/NetBSD/**",
+		"src/SFML/Window/FreeBSD/**",
+	}
+
+	defines {
+		"SFML_STATIC",
+		"_CRT_SECURE_NO_WARNINGS"
+	}
+
+	links {
+		"legacy_stdio_definitions.lib"
+	}
+
+	-- Audio
+	filter "not options:sfml-audio"
+		removefiles {
+			"**/Audio/**",
+		}
+
+	-- Network
+	filter "not options:sfml-network"
+		removefiles {
+			"**/Network/**"
+		}
+
+	-- Windows
 	filter "system:windows"
 		systemversion "latest"
 
 		removefiles { 
-			"**/Android/**,
+			"**/Android/**",
 			"**/iOS/**",
 			"**/Unix/**",
 			"**/OSX/**",
-			"src/SFML/Main/MainAndroid.cpp",
-			"src/SFML/Main/MainiOS.mm",
-			"src/SFML/Main/MainWin32.cpp",
 		}
 
-		defines {
-			"SFML_USE_STATIC_STD_LIBS"
-		}
-
+	-- Debug
 	filter "configurations:Debug"
 		runtime "Debug"
 		symbols "on"
 
+	-- Release
 	filter "configurations:Release"
 		runtime "Release"
 		optimize "on"
